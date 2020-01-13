470601efe0c6271be73cee318c78b2caa41db3c4
Yuchen Liu <eliu0615@gmail.com>
2020-01-13 12:01:19 -0800

    transplant index.html's inline JavaScript code to client.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
@@ -6,6 +7,8 @@ const io = require('socket.io')(http);
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static(__dirname));
 
 let connection_count = 0
 
